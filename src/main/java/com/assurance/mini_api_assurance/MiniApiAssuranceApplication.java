2facbbd40a8f9b254dbde5c2dd32578e700241f3
taher <taher.yahia@isimg.tn>
2026-07-05 15:03:11 +0100

    chore: init spring boot project via spring initializr

---
+++ src/main/java/com/assurance/mini_api_assurance/MiniApiAssuranceApplication.java
@@ -0,0 +1,13 @@
+package com.assurance.mini_api_assurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniApiAssuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniApiAssuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assurance/mini_api_assurance/MiniApiAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assurance.mini_api_assurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApiAssuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
