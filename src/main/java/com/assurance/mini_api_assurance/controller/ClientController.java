8950c4b342034ba8ef5eb0e8b3ab22a56e70b83c
taher <taher.yahia@isimg.tn>
2026-07-06 15:51:41 +0100

    feat: add GET endpoints for Client (list and by id)

--- src/main/java/com/assurance/mini_api_assurance/controller/ClientController.java
+++ src/main/java/com/assurance/mini_api_assurance/controller/ClientController.java
@@ -7,7 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
@@ -22,5 +22,13 @@ public class ClientController {
     public ResponseEntity<ClientResponseDto> creerClient(@Valid @RequestBody ClientCreateDto dto) {
         ClientResponseDto response = clientService.creerClient(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @GetMapping
+    public List<ClientResponseDto> listerClients() {
+        return clientService.listerClients();
+    }
+    @GetMapping("/{id}")
+    public ClientResponseDto getClientParId(@PathVariable Long id) {
+        return clientService.getClientParId(id);
     }
 }
--- src/main/java/com/assurance/mini_api_assurance/service/ClientService.java
+++ src/main/java/com/assurance/mini_api_assurance/service/ClientService.java
@@ -7,7 +7,7 @@ import com.assurance.mini_api_assurance.mapper.ClientMapper;
 import com.assurance.mini_api_assurance.repository.ClientRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.List;
 @Service
 public class ClientService {
 
@@ -32,4 +32,20 @@ public class ClientService {
         // 3. Conversion Entité -> DTO de réponse
         return ClientMapper.toDto(savedClient);
     }
+    @Transactional(readOnly = true)
+    public List<ClientResponseDto>listerClients(){
+        return clientRepository.findAll().stream()
+                .map(ClientMapper::toDto)
+                .toList();
+    }
+    @Transactional(readOnly = true)
+    public ClientResponseDto getClientParId(Long id) {
+        // findById renvoie un Optional. Soit y'a le client, soit y'a rien.
+        // orElseThrow : s'il n'y a rien, on lève une exception immédiatement.
+        Client client = clientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Client non trouvé avec l'id : " + id));
+
+        return ClientMapper.toDto(client);
+    }
+
 }
